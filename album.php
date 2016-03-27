0b88f2279ac73d7a941fc4e7cc0c4d2a1655266c
LO Kam Tao Leo <leolo@leolo.org>
2016-03-27 22:00:26 +0800

    Done album view

--- album.php
+++ album.php
@@ -1,2 +1,96 @@
 <?php
-print_r( $_GET );
+include_once 'common.php';
+$albumDir = $_GET['album'];
+if(!file_exists('data/'.$albumDir.'/info.php'))
+	initAlbum($albumDir);
+include_once 'data/'.$albumDir.'/info.php';
+if(!file_exists('data/'.$albumDir.'/meta'))
+	mkdir('data/'.$albumDir.'/meta');
+$files = scandir('data/'.$albumDir);
+$images = array();
+foreach($files as $file){
+	$ext = strtok( $file , '.');
+	while(true){
+		$token = strtok('.');
+		if($token === FALSE)
+			break;
+		$ext = $token;
+	}
+	$ext = strtolower($ext);
+	if($ext == 'jpg' || $ext == 'png'){
+		$thumbnail = file_exists('data/'.$albumDir.'/meta/'.$file);
+		$info = file_exists('data/'.$albumDir.'/meta/'.$file.'.php');
+		if(!$thumbnail)
+			createThumbnail($albumDir,$file);
+		if(!$info)
+			initImage($albumDir,$file);
+		include 'data/'.$albumDir.'/meta/'.$file.'.php';
+		$image['name'] = $title;
+		$image['created'] = $created;
+		$image['desc'] = $desc;
+		$image['path'] = $file;
+		$images[] = $image;
+	}
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo $albumName;?></title>
+		<script src="jquery-2.2.2.min.js"></script> 
+		<script src="jquery-ui.js"></script> 
+		<link rel="stylesheet" type="text/css" href="jquery-ui.css">
+		<style>
+table.flow{
+	display : inline-block;
+	width : 300px;
+	height : 400px;
+	border-width: 1px;
+	border-color: #ddd;
+	border-style: solid;
+	border-radius: 5px;
+}
+table.flow td{
+	margin-left: auto;
+	margin-right: auto;
+	padding: 5px;
+	width: 290px;
+	height: 390px;
+	vertical-align: top;
+}
+table.flow td img{
+	width: 280px;
+	height: 210px;
+}
+table.flow td div.desc{
+	width: 280px;
+	height: 100px;
+	overflow: auto;
+}
+
+		</style>
+	</head>
+	<body>
+		<h1><?php echo $albumName;?></h1>
+		<div><a href="./">Home</a> &gt; <?php echo $albumName;?></div>
+		<div><?php echo $desc;?></div>
+		<hr>
+		<div>
+<?php foreach($images as $image){ ?>
+<table class="flow">
+	<tr>
+		<td>
+			<a href="<?php echo $albumDir;?>/<?php echo $image['path'];?>">
+				<img src="data/<?php echo $albumDir;?>/meta/<?php echo $image['path'];?>">
+			</a>
+			<hr>
+			<div style="font-weight:bold;"><?php echo $image['name'];?></div>
+			<div style="font-style: italic;">Created <?php echo $image['created'];?></div>
+			<div class="desc"><?php echo $image['desc'];?></div>
+		</td>
+	</tr>
+</table>
+<?php } ?>
+		</div>
+	</body>
+</html>
--- common.php
+++ common.php
@@ -2,6 +2,47 @@
 function initAlbum($dir){
 	$file = fopen('data/'.$dir.'/info.php','w');
 	$date = date('d M Y');
-	fwrite($file, "<?php\n".'$'."albumName = 'Untitled Album';\n".'$'."created = '$date'?>");
+	fwrite($file, "<?php\n".'$'."albumName = 'Untitled Album';\n".'$'."created = '$date';".'$'."desc=\"\";?>");
+	fclose($file);
+}
+function createThumbnail($album,$img){
+	$thumbnail_width = 280;
+	$thumbnail_height = 210;
+	$arr_image_details = getimagesize('data/'.$album.'/'.$img);
+	$original_width = $arr_image_details[0];
+    $original_height = $arr_image_details[1];
+    if ($original_width > $original_height) {
+        $new_width = $thumbnail_width;
+        $new_height = intval($original_height * $new_width / $original_width);
+    } else {
+        $new_height = $thumbnail_height;
+        $new_width = intval($original_width * $new_height / $original_height);
+    }
+    $dest_x = intval(($thumbnail_width - $new_width) / 2);
+    $dest_y = intval(($thumbnail_height - $new_height) / 2);
+    if ($arr_image_details[2] == 1) {
+        $imgt = "ImageGIF";
+        $imgcreatefrom = "ImageCreateFromGIF";
+    }
+    if ($arr_image_details[2] == 2) {
+        $imgt = "ImageJPEG";
+        $imgcreatefrom = "ImageCreateFromJPEG";
+    }
+    if ($arr_image_details[2] == 3) {
+        $imgt = "ImagePNG";
+        $imgcreatefrom = "ImageCreateFromPNG";
+    }
+    if ($imgt) {
+        $old_image = $imgcreatefrom('data/'.$album.'/'.$img);
+        $new_image = imagecreatetruecolor($thumbnail_width, $thumbnail_height);
+        imagecopyresized($new_image, $old_image, $dest_x, $dest_y, 0, 0, $new_width, $new_height, $original_width, $original_height);
+        $imgt($new_image, 'data/'.$album.'/meta/'.$img);
+    }
+    
+}
+function initImage($album,$img){
+	$file = fopen('data/'.$album.'/meta/'.$img.'.php','w');
+	$date = date('d M Y');
+	fwrite($file, "<?php\n".'$'."title = 'Untitled';\n".'$'."created = '$date';".'$'."desc=\"\";?>");
 	fclose($file);
 }
